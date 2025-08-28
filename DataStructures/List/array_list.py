6077e0b7bb659bf52f966ca0b7811a4a975e8e9b
21salazarf <fa.salazar@uniandes.edu.co>
2025-08-28 12:32:50 -0500

    array list corregido

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -50,18 +50,17 @@ def last_element(my_list):
 def remove_first(my_list):
       if my_list["size"] == 0:
         return "IndexError: list index out of range"
-      else:
-        eliminado = my_list["elements"][0]
-        del eliminado
-        my_list["size"] -= 1
-        return eliminado
+      eliminado = my_list["elements"][0]
+      del my_list["elements"][0]
+      my_list["size"] -= 1
+      return eliminado
 
 def remove_last(my_list):
     if my_list["size"] == 0:
         return "IndexError: list index out of range"
     else:
         eliminado = my_list["elements"][-1]
-        del eliminado
+        del my_list["elements"][-1]
         my_list["size"] -= 1
         return eliminado
 
@@ -76,8 +75,8 @@ def insert_element(my_list, pos, element):
 def change_info(my_list, pos, new_info):
     if pos < 0 or pos >= my_list["size"]:
         return "IndexError: list index out of range"
-    else:
-        my_list["elements"][pos] = new_info
+    my_list["elements"][pos] = new_info
+    return my_list
         
 def exchange(my_list, pos_1, pos_2):
     if (pos_1 < 0 or pos_1 >= my_list["size"]) or (pos_2 < 0 or pos_2 >= my_list["size"]):
@@ -87,6 +86,8 @@ def exchange(my_list, pos_1, pos_2):
         my_list["elements"][pos_2], 
         my_list["elements"][pos_1]
     )
+    
+    return my_list
         
 def sub_list(my_list, pos_i, num_elements):
     if pos_i < 0 or pos_i >= my_list["size"]:
@@ -104,4 +105,13 @@ def sub_list(my_list, pos_i, num_elements):
     }
     
     
-    
+def is_empty(my_list):
+    return my_list["size"] == 0
+
+def delete_element(my_list, pos):
+    if pos < 0 or pos >= my_list["size"]:
+        return "IndexError: list index out of range"
+    del my_list["elements"][pos]
+    my_list["size"] -= 1
+    return my_list
+
